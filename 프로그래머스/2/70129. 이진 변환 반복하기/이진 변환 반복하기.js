7cc1b79133ad23b50b87d76a1b83efb04f635dc1
KJWHUB <126055193+KJWHUB@users.noreply.github.com>
2023-10-15 19:14:47 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 4.50 ms, Memory: 34.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.js
@@ -0,0 +1,18 @@
+function solution(s) {
+    let str = s.slice()
+    var answer = [0, 0];
+    
+    while(str !== '1') {
+        answer[0]++
+        
+        const L = str.length
+        const deleteZeroL = str.replaceAll('0', '').length
+        
+        answer[1] += L - deleteZeroL
+        
+        str = deleteZeroL.toString(2)
+        console.log(str)
+    }
+    
+    return answer;
+}
